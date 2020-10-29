17c8f3d9c4d6321b613837644223443b719f6880
manueltov <manuel@tovar.pt>
2020-10-29 11:47:01 +0000

    repor testes prof

--- source/test_data.c
+++ source/test_data.c
@@ -1,8 +1,3 @@
-// SD-041
-// João Figueiredo, nº 53524
-// Manuel Tovar, nº 49522
-// Mariana Bento, nº 53676
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -13,13 +8,12 @@
 
 void pee(const char *msg)
 {
-	perror(msg);
-	exit(0);
+    perror(msg);
+    exit(0);
 }
 
 /**************************************************************/
-int testCreate()
-{
+int testCreate() {
 	int result;
 	struct data_t *data;
 
@@ -34,32 +28,32 @@ int testCreate()
 	if ((data = data_create(1024)) == NULL)
 		pee("  O teste não pode prosseguir");
 
-	memcpy(data->data, "1234567890a", strlen("1234567890a") + 1);
+	memcpy(data->data,"1234567890a",strlen("1234567890a")+1);
 
 	result = result &&
-			 ((strcmp(data->data, "1234567890a") == 0) && (data->datasize == 1024));
+		 ((strcmp(data->data,"1234567890a") == 0) && (data->datasize == 1024));
 
 	data_destroy(data);
 
-	printf("data - testCreate: %s\n", result ? "passou" : "não passou");
+	printf("data - testCreate: %s\n",result?"passou":"não passou");
 	return result;
 }
 
 /**************************************************************/
-int testCreate2()
-{
+int testCreate2() {
 	int result, data_size;
 	struct data_t *data;
 	//char *data_s = "1234567890abc";
 	char *data_s = strdup("1234567890abc");
-	data_size = strlen(data_s) + 1;
+	data_size = strlen(data_s)+1;
+
 
 	printf("Módulo data -> teste data_create2:");
 
 	assert(data_create2(-1, data_s) == NULL);
 	result = (data_create2(-1, data_s) == NULL);
 
-	assert(data_create2(0, data_s) == NULL);
+    	assert(data_create2(0, data_s) == NULL);
 	result = result && (data_create2(0, data_s) == NULL);
 
 	assert(data_create2(data_size, NULL) == NULL);
@@ -71,31 +65,31 @@ int testCreate2()
 	result = result && (data->data == data_s);
 
 	result = result &&
-			 ((strcmp(data->data, data_s) == 0) && (data->datasize == data_size));
+		 ((strcmp(data->data, data_s) == 0) && (data->datasize == data_size));
 
 	data_destroy(data);
 
-	printf("data - testCreate2: %s\n", result ? "passou" : "não passou");
+	printf("data - testCreate2: %s\n",result?"passou":"não passou");
 	return result;
 }
 
+
 /**************************************************************/
 
-int testCreate3()
-{
+int testCreate3() {
 	int result, data_size;
 	struct data_t *data;
 	char *data_s = "1234567890abc";
-	data_size = strlen(data_s) + 1;
-	char *data_p = malloc(data_size);
-	strcpy(data_p, data_s);
+	data_size = strlen(data_s)+1;
+	char *data_p = malloc (data_size);
+	strcpy (data_p, data_s);
 
-	printf("Módulo data -> teste data_create3:");
+	printf("Módulo data -> teste data_create2:");
 
 	assert(data_create2(-1, data_p) == NULL);
 	result = (data_create2(-1, data_p) == NULL);
 
-	assert(data_create2(0, data_p) == NULL);
+    	assert(data_create2(0, data_p) == NULL);
 	result = result && (data_create2(0, data_p) == NULL);
 
 	assert(data_create2(data_size, NULL) == NULL);
@@ -106,21 +100,21 @@ int testCreate3()
 
 	result = result && (data->data == data_p);
 
-	result = result && ((strcmp(data->data, data_p) == 0) && (data->datasize == data_size));
+	result = result &&
+		 ((strcmp(data->data, data_p) == 0) && (data->datasize == data_size));
 
 	data_destroy(data);
 
-	printf("data - testCreate3: %s\n", result ? "passou" : "não passou");
+	printf("data - testCreate3: %s\n",result?"passou":"não passou");
 	return result;
 }
 
 /**************************************************************/
 
-int testDup()
-{
+int testDup() {
 	//char *data_s = "1234567890abc";
 	char *data_s = strdup("1234567890abc");
-	int result, data_size = strlen(data_s) + 1;
+	int result, data_size = strlen(data_s)+1;
 	struct data_t *data;
 	struct data_t *data2;
 
@@ -128,6 +122,7 @@ int testDup()
 
 	assert(data_dup(NULL) == NULL);
 	result = (data_dup(NULL) == NULL);
+
 	if ((data = data_create(1)) == NULL)
 		pee("  O teste não pode prosseguir");
 
@@ -135,76 +130,78 @@ int testDup()
 
 	assert(data_dup(data) == NULL);
 	result = result && (data_dup(data) == NULL);
-	free(data->data);
 
+	free(data->data);
+	
 	data->data = NULL;
 	assert(data_dup(data) == NULL);
 	result = result && (data_dup(data) == NULL);
-
+	
 	data->datasize = 1;
 	assert(data_dup(data) == NULL);
 	result = result && (data_dup(data) == NULL);
 
 	free(data);
 
-	if ((data = data_create2(data_size, data_s)) == NULL)
+	if ((data = data_create2(data_size,data_s)) == NULL)
 		pee("  O teste não pode prosseguir");
-
+	
 	data2 = data_dup(data);
-
+	
 	result = result && (data2 != data);
 
-	result = result && (data->data != data2->data) &&
-			 (data->datasize == data2->datasize) &&
-			 (memcmp(data->data, data2->data, data->datasize) == 0);
+	result = result && (data->data != data2->data) && 
+                 (data->datasize == data2->datasize) &&
+                 (memcmp(data->data, data2->data, data->datasize) == 0);
 
 	data_destroy(data);
 	data_destroy(data2);
 
-	printf("data - testDup: %s\n", result ? "passou" : "não passou");
+	printf("data - testDup: %s\n",result?"passou":"não passou");
 	return result;
 }
 
+
 /**************************************************************/
-int testDestroy()
-{
-	printf("Módulo data -> teste data_destroy:");
+int testDestroy(){
+    printf("Módulo data -> teste data_destroy:");
 	data_destroy(NULL);
-	printf(" passou\n");
+    printf(" passou\n");
 	return 1;
 }
 
+
 /**************************************************************/
-int testReplace()
-{
+int testReplace(){
 	int result, data_size, data_size2;
 	struct data_t *data;
 	//char *data_s = "1234567890abc";
 	char *data_s = strdup("123");
 	char *data_s2 = strdup("123456");
-	data_size = strlen(data_s) + 1;
-	data_size2 = strlen(data_s2) + 1;
+	data_size = strlen(data_s)+1;
+	data_size2 = strlen(data_s2)+1;
+
 
 	printf("Módulo data -> teste testReplace:");
 
 	data = data_create2(data_size, data_s);
-
+	
 	data_replace(data, data_size2, data_s2);
 
 	result = (data->data == data_s2);
 
 	result = result &&
-			 ((strcmp(data->data, data_s2) == 0) && (data->datasize == data_size2));
+		 ((strcmp(data->data, data_s2) == 0) && (data->datasize == data_size2));
 
 	data_destroy(data);
 
-	printf("data - testCreate2: %s\n", result ? "passou" : "não passou");
+	printf("data - testCreate2: %s\n",result?"passou":"não passou");
 	return result;
 }
 
+
 /**************************************************************/
-int main()
-{
+int main() {
 	int score = 0;
 
 	printf("\nIniciando o teste do módulo data \n");
@@ -212,19 +209,19 @@ int main()
 	score += testCreate();
 
 	score += testCreate2();
-
+	
 	score += testCreate3();
 
 	score += testDup();
-
+	
 	score += testDestroy();
-
+	
 	score += testReplace();
 
-	printf("teste data bin: %d/6\n", score);
+	printf("teste data bin: %d/6\n",score);
 
-	if (score == 6)
-		return 0;
-	else
-		return -1;
+    if (score == 6)
+        return 0;
+    else
+        return -1;
 }
--- source/test_tree.c
+++ source/test_tree.c
@@ -1,8 +1,3 @@
-// SD-041
-// João Figueiredo, nº 53524
-// Manuel Tovar, nº 49522
-// Mariana Bento, nº 53676
-
 #define NDEBUG
 
 #include <stdlib.h>
@@ -15,155 +10,127 @@
 #include "tree.h"
 
 /**************************************************************/
-int testTreeVazia()
-{
+int testTreeVazia() {
 	struct tree_t *tree = tree_create();
 
 	int result = tree != NULL && tree_size(tree) == 0;
 
 	tree_destroy(tree);
-
-	printf("tree - testTreeVazia: %s\n", result ? "passou" : "não passou");
+	
+	printf("tree - testTreeVazia: %s\n",result?"passou":"não passou");
 	return result;
 }
 
 /**************************************************************/
 
-int testPutInexistente()
-{
+int testPutInexistente() {
 	int result, i;
 	struct tree_t *tree = tree_create();
 	char *key[1024];
 	struct data_t *data[1024], *d;
 
-	//data [struct data_t,struct data_t,struct data_t,...,struct data_t]
-	//d = null
+	for(i=0; i<1024; i++) {
+		key[i] = (char*)malloc(16*sizeof(char));
+		sprintf(key[i],"a/key/b-%d",i);
+		data[i] = data_create2(strlen(key[i])+1,strdup(key[i]));
 
-	for (i = 0; i < 1024; i++)
-	{
-		key[i] = (char *)malloc(16 * sizeof(char));
-		sprintf(key[i], "a/key/b-%d", i);
-		data[i] = data_create2(strlen(key[i]) + 1, strdup(key[i]));
-		tree_put(tree, key[i], data[i]);
+		tree_put(tree,key[i],data[i]);
 	}
+
 	assert(tree_size(tree) == 1024);
 	result = (tree_size(tree) == 1024);
-	printf("result: %d\n", result);
 
-	for (i = 0; i < 1024; i++)
-	{
-		printf("aaaaa\n");
-		d = tree_get(tree, key[i]);
-		printf("11111\n");
+	for(i=0; i<1024; i++) {
+		d = tree_get(tree,key[i]);
+
 		assert(d->datasize == data[i]->datasize);
-		printf("22222\n");
-		assert(memcmp(d->data, data[i]->data, d->datasize) == 0);
-		printf("33333\n");
+		assert(memcmp(d->data,data[i]->data,d->datasize) == 0);
 		assert(d->data != data[i]->data);
-		printf("44444\n");
-		// result = result && (d->datasize == data[i]->datasize &&
-		// 					memcmp(d->data, data[i]->data, d->datasize) == 0 &&
-		// 					d->data != data[i]->data);
-		printf("55555\n");
-		int c = d->data != data[i]->data;
-		printf("c: %d\n", c);
-		int b = memcmp(d->data, data[i]->data, d->datasize) == 0 && c;
-		// printf("b: %d\n", b);
-		int a = (d->datasize == data[i]->datasize && b);
-		// printf("a: %d\n", a);
-		result = result && a;
-		// printf("interior result: %d\n", result);
+
+		result = result && (d->datasize == data[i]->datasize && 
+                           memcmp(d->data,data[i]->data,d->datasize) == 0 &&
+                           d->data != data[i]->data);
 		data_destroy(d);
 	}
 
-	printf("result: %d\n", result);
-	for (i = 0; i < 1024; i++)
-	{
+	for(i=0; i<1024; i++) {
 		free(key[i]);
 		data_destroy(data[i]);
 	}
 
 	tree_destroy(tree);
-
-	printf("tree - testPutInexistente: %s\n", result ? "passou" : "não passou");
+	
+	printf("tree - testPutInexistente: %s\n",result?"passou":"não passou");
 	return result;
 }
 
 /**************************************************************/
 
-int testPutExistente()
-{
+int testPutExistente() {
 	int result, i;
 	struct tree_t *tree = tree_create();
 	char *key[1024];
 	struct data_t *data[1024], *d;
 
-	for (i = 0; i < 1024; i++)
-	{
-		key[i] = (char *)malloc(16 * sizeof(char));
-		sprintf(key[i], "a/key/b-%d", i);
-		data[i] = data_create2(strlen(key[i]) + 1, strdup(key[i]));
+	for(i=0; i<1024; i++) {
+		key[i] = (char*)malloc(16*sizeof(char));
+		sprintf(key[i],"a/key/b-%d",i);
+		data[i] = data_create2(strlen(key[i])+1,strdup(key[i]));
 
-		tree_put(tree, key[i], data[i]);
+		tree_put(tree,key[i],data[i]);
 	}
+
 
 	assert(tree_size(tree) == 1024);
 	result = (tree_size(tree) == 1024);
 
-	d = data_create2(strlen("256") + 1, strdup("256"));
-	tree_put(tree, key[256], d);
+	d = data_create2(strlen("256")+1,strdup("256"));
+	tree_put(tree,key[256],d);
 	data_destroy(d);
 
 	assert(tree_size(tree) == 1024);
 	result = result && (tree_size(tree) == 1024);
 
-	for (i = 0; i < 1024; i++)
-	{
-		d = tree_get(tree, key[i]);
-
-		if (i == 256)
-		{
-			result = result && (d->datasize == strlen("256") + 1 &&
-								memcmp(d->data, "256", d->datasize) == 0);
-		}
-		else
-		{
-			result = result && (d->datasize == data[i]->datasize &&
-								memcmp(d->data, data[i]->data, d->datasize) == 0 &&
-								d->data != data[i]->data);
+	for(i=0; i<1024; i++) {
+		d = tree_get(tree,key[i]);
+		
+		if(i==256) {
+			result = result && (d->datasize == strlen("256")+1 && 
+        	                   memcmp(d->data,"256",d->datasize) == 0);
+		} else {
+			result = result && (d->datasize == data[i]->datasize && 
+        	                   memcmp(d->data,data[i]->data,d->datasize) == 0 &&
+        	                   d->data != data[i]->data);
 		}
 
 		data_destroy(d);
 	}
 
-	for (i = 0; i < 1024; i++)
-	{
+	for(i=0; i<1024; i++) {
 		free(key[i]);
 		data_destroy(data[i]);
 	}
 
 	tree_destroy(tree);
-
-	printf("tree - testPutExistente: %s\n", result ? "passou" : "não passou");
+	
+	printf("tree - testPutExistente: %s\n",result?"passou":"não passou");
 	return result;
 }
 
 /**************************************************************/
 
-int testDelInexistente()
-{
+int testDelInexistente() {
 	int result, i;
 	struct tree_t *tree = tree_create();
 	char *key;
 	struct data_t *data;
 
-	for (i = 0; i < 1024; i++)
-	{
-		key = (char *)malloc(16 * sizeof(char));
-		sprintf(key, "a/key/b-%d", i);
-		data = data_create2(strlen(key) + 1, key);
+	for(i=0; i<1024; i++) {
+		key = (char*)malloc(16*sizeof(char));
+		sprintf(key,"a/key/b-%d",i);
+		data = data_create2(strlen(key)+1,key);
 
-		tree_put(tree, key, data);
+		tree_put(tree,key,data);
 
 		data_destroy(data);
 	}
@@ -171,40 +138,38 @@ int testDelInexistente()
 	assert(tree_size(tree) == 1024);
 	result = (tree_size(tree) == 1024);
 
-	result = result && (tree_get(tree, "a/key/b-1024") == NULL) &&
-			 (tree_get(tree, "abc") == NULL);
+	result = result && (tree_get(tree,"a/key/b-1024") == NULL) &&
+			   (tree_get(tree,"abc") == NULL);
 
-	result = result && (tree_del(tree, "a/key/b-1024") != 0) &&
-			 (tree_del(tree, "abc") != 0);
+	result = result && (tree_del(tree,"a/key/b-1024") != 0) &&
+			   (tree_del(tree,"abc") != 0);
 
-	result = result && (tree_get(tree, "a/key/b-1024") == NULL) &&
-			 (tree_get(tree, "abc") == NULL);
+	result = result && (tree_get(tree,"a/key/b-1024") == NULL) &&
+			   (tree_get(tree,"abc") == NULL);
 
 	assert(tree_size(tree) == 1024);
 	result = result && (tree_size(tree) == 1024);
 
 	tree_destroy(tree);
-
-	printf("tree - testDelInexistente: %s\n", result ? "passou" : "não passou");
+	
+	printf("tree - testDelInexistente: %s\n",result?"passou":"não passou");
 	return result;
 }
 
 /**************************************************************/
 
-int testDelExistente()
-{
+int testDelExistente() {
 	int result, i;
 	struct tree_t *tree = tree_create();
 	char *key;
 	struct data_t *data, *data2 = NULL;
 
-	for (i = 0; i < 1024; i++)
-	{
-		key = (char *)malloc(16 * sizeof(char));
-		sprintf(key, "a/key/b-%d", i);
-		data = data_create2(strlen(key) + 1, key);
+	for(i=0; i<1024; i++) {
+		key = (char*)malloc(16*sizeof(char));
+		sprintf(key,"a/key/b-%d",i);
+		data = data_create2(strlen(key)+1,key);
 
-		tree_put(tree, key, data);
+		tree_put(tree,key,data);
 
 		data_destroy(data);
 	}
@@ -212,52 +177,49 @@ int testDelExistente()
 	assert(tree_size(tree) == 1024);
 	result = (tree_size(tree) == 1024);
 
-	result = result && ((data = tree_get(tree, "a/key/b-1023")) != NULL) &&
-			 ((data2 = tree_get(tree, "a/key/b-45")) != NULL);
+	result = result && ((data = tree_get(tree,"a/key/b-1023")) != NULL) &&
+			   ((data2 = tree_get(tree,"a/key/b-45")) != NULL);
 
 	data_destroy(data);
 	data_destroy(data2);
 
-	result = result && (tree_del(tree, "a/key/b-1023") == 0) &&
-			 (tree_del(tree, "a/key/b-45") == 0);
+	result = result && (tree_del(tree,"a/key/b-1023") == 0) &&
+			   (tree_del(tree,"a/key/b-45") == 0);
 
-	result = result && (tree_get(tree, "a/key/b-1023") == NULL) &&
-			 (tree_get(tree, "a/key/b-45") == NULL);
+	result = result && (tree_get(tree,"a/key/b-1023") == NULL) &&
+			   (tree_get(tree,"a/key/b-45") == NULL);
 
 	assert(tree_size(tree) == 1022);
 	result = result && (tree_size(tree) == 1022);
 
 	tree_destroy(tree);
-
-	printf("tree - testDelExistente: %s\n", result ? "passou" : "não passou");
+	
+	printf("tree - testDelExistente: %s\n",result?"passou":"não passou");
 	return result;
 }
 
 /**************************************************************/
 
-int testGetKeys()
-{
-	int result = 1, i, j, achou;
+int testGetKeys() {
+	int result = 1,i,j,achou;
 	struct tree_t *tree = tree_create();
 	char **keys;
-	char *k[4] = {"abc", "bcd", "cde", "def"};
+	char *k[4] = {"abc","bcd","cde","def"};
 	struct data_t *d = data_create(5);
 
-	tree_put(tree, k[3], d);
-	tree_put(tree, k[2], d);
-	tree_put(tree, k[1], d);
-	tree_put(tree, k[0], d);
+	tree_put(tree,k[3],d);
+	tree_put(tree,k[2],d);
+	tree_put(tree,k[1],d);
+	tree_put(tree,k[0],d);
 
 	data_destroy(d);
 
 	keys = tree_get_keys(tree);
-
-	for (i = 0; keys[i] != NULL; i++)
-	{
+	
+	for(i=0; keys[i] != NULL; i++) {
 		achou = 0;
-		for (j = 0; j < 4; j++)
-		{
-			achou = (achou || (strcmp(keys[i], k[j]) == 0));
+		for(j=0; j<4; j++) {
+			achou = (achou || (strcmp(keys[i],k[j]) == 0));
 		}
 		result = (result && achou);
 	}
@@ -268,14 +230,13 @@ int testGetKeys()
 
 	tree_destroy(tree);
 
-	printf("tree - testGetKeys: %s\n", result ? "passou" : "não passou");
+	printf("tree - testGetKeys: %s\n",result?"passou":"não passou");
 	return result;
 }
 
 /**************************************************************/
 
-int main()
-{
+int main() {
 	int score = 0;
 
 	printf("iniciando teste tree bin\n");
@@ -291,10 +252,11 @@ int main()
 	score += testDelExistente();
 
 	score += testGetKeys();
-	printf("teste tree bin: %d/6\n", score);
 
-	if (score == 6)
-		return 0;
-	else
-		return -1;
+	printf("teste tree bin: %d/6\n",score);
+
+    if (score == 6)
+        return 0;
+    else
+        return -1;
 }
